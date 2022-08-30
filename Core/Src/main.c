eb77e83c192252571dfe2e2749fe3a0c774153bb
asethi <asethi@OY2108047.univ.yo.oulu.fi>
2022-08-30 21:06:08 +0300

    blinking red led in errror case, lot of changes cause of auto indentation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,66 +2,58 @@
 #include "cmsis_os.h"
 #include "lwip.h"
 
-
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-static void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+static void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 168;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
-
-void vLWIPTask( UBaseType_t uxPriority )
-{
-	  MX_LWIP_Init();
-	  vStartMlabHandlerTask( MATLAB_HANLDER_TASK_PRIORITY);
-	  while(1)
-	  {
-		  taskYIELD();
-	  }
-	  return;
+void vLWIPTask(UBaseType_t uxPriority) {
+	MX_LWIP_Init();
+	vStartMlabHandlerTask( MATLAB_HANLDER_TASK_PRIORITY);
+	while (1) {
+		taskYIELD();
+	}
+	return;
 }
 
-int main(void)
-{
+int main(void) {
 	BaseType_t xReturned;
 
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
@@ -70,71 +62,69 @@ int main(void)
 	SystemClock_Config();
 	trace_init();
 
-   /* Initialize all configured peripherals */
+	/* Initialize all configured peripherals */
 	board_init();
 
 //  MX_LWIP_Init();
-	xReturned = xTaskCreate( vLWIPTask, "LWIPx", LWIP_INIT_STACK_SIZE, NULL, LWIP_INIT_TASK_PRIORITY, ( TaskHandle_t * ) NULL );
-	if( xReturned != pdPASS )
-	{
+	xReturned = xTaskCreate(vLWIPTask, "LWIPx", LWIP_INIT_STACK_SIZE, NULL,
+			LWIP_INIT_TASK_PRIORITY, (TaskHandle_t*) NULL);
+	if (xReturned != pdPASS) {
 		/* The task was created.  Use the task's handle to delete the task. */
 		trace_printf("failed to create the LWIP init task\n");
 	}
 
-  vStartLEDFlashTasks( LED_FLASH_TASK_PRIORITY);
-//  	vStartMlabHandlerTask( MATLAB_HANLDER_TASK_PRIORITY);
-//  	vStartChipRegTask( CHIP_REG_HANDLER_TASK_PRIORITY);
+	vStartLEDFlashTasks( LED_FLASH_TASK_PRIORITY);
+	vStartChipRegTask( CHIP_REG_HANDLER_TASK_PRIORITY);
 //  	vStartSynthTask( SYNTH_TASK_PRIORITY);
 //  	vStartPotTask( POT_TASK_PRIORITY);
 //  	vStartSensorTask( SENSOR_TASK_PRIORITY);
 
-  /* Start scheduler */
-  osKernelStart();
+	/* Start scheduler */
+	osKernelStart();
 
-  /* We should never get here as control is now taken by the scheduler */
-  while (1)
-  {
-  }
+	/* We should never get here as control is now taken by the scheduler */
+	while (1) {
+	}
 
 }
 
-
-
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	/* USER CODE BEGIN Callback 0 */
 
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM1) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
 
-  /* USER CODE END Callback 1 */
+	/* USER CODE END Callback 1 */
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  trace_printf("error handler");
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	board_green_led_off();
+	board_blue_led_off();
+	trace_printf("error handler");
+	while (1) {
+		board_red_led_toggle();
+		for (uint32_t i = 0; i < 5000000; i++)
+			;
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
