7ea01726ed864c3eac645193c5587aa5f0813c6b
asethi <asethi@OY2108047.univ.yo.oulu.fi>
2022-09-12 22:16:48 +0300

    added code for board shift register

---
+++ Core/Inc/prog_board_shreg.h
@@ -0,0 +1,47 @@
+/*
+ * prog_board_shreg.h
+ *
+ *  Created on: Sep 5, 2022
+ *      Author: asethi
+ */
+
+#ifndef INC_PROG_BOARD_SHREG_H_
+#define INC_PROG_BOARD_SHREG_H_
+
+#define BOARD_SHREG_LEN			(80)
+#define BOARD_CLK_DELAY			(100)
+
+#define IREF1_ICn_EN_IDX(n) 	(2*n-2) /*offset by -1*/
+#define IREF2_ICn_EN_IDX(n) 	(2*n-1)
+#define BB_AMP_IC6_I_EN_IDX		(32)
+#define BB_AMP_IC1_I_EN_IDX 	(33)
+#define BB_AMP_IC16_I_EN_IDX	(34)
+#define BB_AMP_IC11_I_EN_IDX 	(35)
+#define BB_AMP_IC6_Q_EN_IDX		(36)
+#define BB_AMP_IC1_Q_EN_IDX 	(37)
+#define BB_AMP_IC16_Q_EN_IDX 	(38)
+#define BB_AMP_IC11_Q_EN_IDX 	(39)
+#define RX_BB_SW_IC1_I_EN_IDX	(40)
+#define RX_BB_SW_IC6_I_EN_IDX 	(41)
+#define RX_BB_SW_IC11_I_EN_IDX  (42)
+#define RX_BB_SW_IC16_I_EN_IDX  (43)
+#define RX_BB_SW_IC1_Q_EN_IDX  	(44)
+#define RX_BB_SW_IC6_Q_EN_IDX  	(45)
+#define RX_BB_SW_IC11_Q_EN_IDX  (46)
+#define RX_BB_SW_IC16_Q_EN_IDX 	(47)
+#define VDD_1V5_EN_IDX 			(48)
+#define VDD_1V_R1_EN_IDX		(49)
+#define VDD_1V_R2_EN_IDX		(50)
+#define VDD_1V_R3_EN_IDX		(51)
+#define VDD_1V_R4_EN_IDX		(52)
+#define VDD_2V75_EN_IDX			(53)
+#define REFCLK_SYNTH1_EN_IDX	(54)
+#define REFCLK_SYNTH2_EN_IDX	(55)
+#define PIN_LE_IDX(n) 			(n>=9?(56+n-9):(65+n))
+
+void shreg_set_1V0_en(uint8_t en, pow_plane_t plane);
+void shreg_set_1V5_en(uint8_t en);
+void shreg_set_2V75_en(uint8_t en);
+void shreg_set_le(uint8_t ic_id);
+
+#endif /* INC_PROG_BOARD_SHREG_H_ */
--- Core/Src/prog_board_shreg.c
+++ Core/Src/prog_board_shreg.c
@@ -10,41 +10,54 @@
 #include "main.h"
 #include "semphr.h"
 #include <string.h>
-#define BOARD_SHREG_LEN			(80)
-
-#define IREF1_ICn_EN_IDX(n) 	(2*n-2) /*offset by -1*/
-#define IREF2_ICn_EN_IDX(n) 	(2*n-1)
-#define BB_AMP_IC6_I_EN_IDX		(32)
-#define BB_AMP_IC1_I_EN_IDX 	(33)
-#define BB_AMP_IC16_I_EN_IDX	(34)
-#define BB_AMP_IC11_I_EN_IDX 	(35)
-#define BB_AMP_IC6_Q_EN_IDX		(36)
-#define BB_AMP_IC1_Q_EN_IDX 	(37)
-#define BB_AMP_IC16_Q_EN_IDX 	(38)
-#define BB_AMP_IC11_Q_EN_IDX 	(39)
-#define RX_BB_SW_IC1_I_EN_IDX	(40)
-#define RX_BB_SW_IC6_I_EN_IDX 	(41)
-#define RX_BB_SW_IC11_I_EN_IDX  (42)
-#define RX_BB_SW_IC16_I_EN_IDX  (43)
-#define RX_BB_SW_IC1_Q_EN_IDX  	(44)
-#define RX_BB_SW_IC6_Q_EN_IDX  	(45)
-#define RX_BB_SW_IC11_Q_EN_IDX  (46)
-#define RX_BB_SW_IC16_Q_EN_IDX 	(47)
-#define VDD_1V5_EN_IDX 			(48)
-#define VDD_1V_R1_EN_IDX		(49)
-#define VDD_1V_R2_EN_IDX		(50)
-#define VDD_1V_R3_EN_IDX		(51)
-#define VDD_1V_R4_EN_IDX		(52)
-#define VDD_2V75_EN_IDX			(53)
-#define REFCLK_SYNTH1_EN_IDX	(54)
-#define REFCLK_SYNTH2_EN_IDX	(55)
-#define PIN_LE_IDX(n) 			(n>=9?(56+n-9):(65+n))
 
 static uint8_t g_shreg_data[BOARD_SHREG_LEN];
 
-void fill_def_values(void)
-{
-	for (uint8_t i=0;i<BOARD_SHREG_LEN;i++)
+static void clkout_data(void) {
+	GPIO_TypeDef *p_dport, *p_clkport, *p_lport;
+	uint16_t dpin, clkpin, lpin;
+
+	p_dport = SHREG_DATA_GPIO_Port;
+	p_clkport = SHREG_CLK_GPIO_Port;
+	p_lport = SHREG_LATCH_GPIO_Port;
+
+	dpin = SHREG_DATA_Pin;
+	clkpin = SHREG_CLK_Pin;
+	lpin = SHREG_LATCH_Pin;
+
+	for (int8_t i = BOARD_SHREG_LEN - 1; i >= 0; i--) {
+		uint8_t data;
+
+		data = g_shreg_data[i] & 0x1;
+
+		if (data)
+			HAL_GPIO_WritePin(p_dport, dpin, GPIO_PIN_SET);
+		else
+			HAL_GPIO_WritePin(p_dport, dpin, GPIO_PIN_RESET);
+
+		for (int i = 0; i < BOARD_CLK_DELAY / 2; i++)
+			;
+		HAL_GPIO_WritePin(p_clkport, clkpin, GPIO_PIN_SET);
+		for (int i = 0; i < BOARD_CLK_DELAY; i++)
+			;
+		HAL_GPIO_WritePin(p_clkport, clkpin, GPIO_PIN_RESET);
+		for (int i = 0; i < BOARD_CLK_DELAY / 2; i++)
+			;
+	}
+	// send the LE clk
+	for (int i = 0; i < BOARD_CLK_DELAY / 2; i++)
+		;
+	HAL_GPIO_WritePin(p_lport, lpin, GPIO_PIN_SET);
+	for (int i = 0; i < BOARD_CLK_DELAY; i++)
+		;
+	HAL_GPIO_WritePin(p_lport, lpin, GPIO_PIN_RESET);
+	for (int i = 0; i < BOARD_CLK_DELAY / 2; i++)
+		;
+
+	return;
+}
+static void fill_def_values(void) {
+	for (uint8_t i = 0; i < BOARD_SHREG_LEN; i++)
 		g_shreg_data[i] = 0;
 
 	// set active low controls to 1
@@ -59,27 +72,85 @@ void fill_def_values(void)
 	return;
 }
 
-void init_shreg(void)
-{
-	GPIO_TypeDef *p_dport, *p_clkport, *p_lport;
-		uint16_t dpin, clkpin, lpin;
+void shreg_set_1V0_en(uint8_t en, pow_plane_t plane) {
+	uint8_t idx;
+	switch (plane) {
+	case R1:
+		idx = VDD_1V_R1_EN_IDX;
+		break;
+	case R2:
+		idx = VDD_1V_R2_EN_IDX;
+		break;
+	case R3:
+		idx = VDD_1V_R3_EN_IDX;
+		break;
+	case R4:
+		idx = VDD_1V_R4_EN_IDX;
+		break;
 
-		p_dport = SHREG_DATA_GPIO_Port;
-		p_clkport = SHREG_CLK_GPIO_Port;
-		p_lport = SHREG_LATCH_GPIO_Port;
-
-		dpin = SHREG_DATA_Pin;
-		clkpin = SHREG_CLK_Pin;
-		lpin = SHREG_LATCH_Pin;
-
-	for (int8_t i=BOARD_SHREG_LEN-1;i>0; i--)
-	{
-		uint8_t data;
-
-		data = g_shreg_data[i] & 0x1;
-
-//		HAL_GPIO_WritePin(p_port, pin, GPIO_PIN_RESET);
-
-
+	default:
+		trace_printf("invalid plane");
+		Error_Handler();
+		break;
 	}
+
+	if (en)
+		g_shreg_data[idx] = 0;  // enable is active low
+	else
+		g_shreg_data[idx] = 1;
+
+	clkout_data();
+	return;
+}
+
+void shreg_set_1V5_en(uint8_t en) {
+	if (en)
+		g_shreg_data[VDD_1V5_EN_IDX] = 1;  // enable is active high
+	else
+		g_shreg_data[VDD_1V5_EN_IDX] = 0;
+
+	clkout_data();
+	return;
+}
+
+void shreg_set_2V75_en(uint8_t en) {
+	if (en)
+		g_shreg_data[VDD_2V75_EN_IDX] = 1;  // enable is active high
+	else
+		g_shreg_data[VDD_2V75_EN_IDX] = 0;
+
+	clkout_data();
+	return;
+}
+
+void shreg_set_le(uint8_t ic_id) {
+	uint8_t idx;
+	idx = PIN_LE_IDX(ic_id);
+
+	g_shreg_data[idx] = 1;
+	clkout_data();
+	g_shreg_data[idx] = 0;
+	clkout_data();
+	return;
+}
+
+void init_shreg(void) {
+	GPIO_TypeDef *p_dport, *p_clkport, *p_lport;
+	uint16_t dpin, clkpin, lpin;
+
+	p_dport = SHREG_DATA_GPIO_Port;
+	p_clkport = SHREG_CLK_GPIO_Port;
+	p_lport = SHREG_LATCH_GPIO_Port;
+
+	dpin = SHREG_DATA_Pin;
+	clkpin = SHREG_CLK_Pin;
+	lpin = SHREG_LATCH_Pin;
+
+	HAL_GPIO_WritePin(p_dport, dpin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(p_clkport, clkpin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(p_lport, lpin, GPIO_PIN_RESET);
+
+	fill_def_values();
+	clkout_data();
+	return;
 }
